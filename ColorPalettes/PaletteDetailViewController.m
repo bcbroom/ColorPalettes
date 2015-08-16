38a118daeb6f96c017fcf154d7672a0fdc8ae931
Brian Broom <brian.broom@gmail.com>
2015-08-16 18:54:49 -0400

    Detail VC draws rectangle pattern with colors from selected palette.

--- ColorPalettes/PaletteDetailViewController.m
+++ ColorPalettes/PaletteDetailViewController.m
@@ -16,6 +16,33 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+  [self drawColorRects];
+}
+
+- (void)drawColorRects {
+  CGRect largeRect = self.view.bounds;
+  CGRect sliceRect;
+  CGRect leftoverRect;
+  
+  for (UIColor *color in self.selectedPalette.colors) {
+    CGFloat height = largeRect.size.height;
+    CGFloat width = largeRect.size.width;
+    
+    if (color != self.selectedPalette.colors.lastObject) {
+      if (height > width) {
+        CGRectDivide(largeRect, &sliceRect, &leftoverRect, height / 2, CGRectMinYEdge);
+      } else {
+        CGRectDivide(largeRect, &sliceRect, &leftoverRect, width / 2, CGRectMinXEdge);
+      }
+    } else {
+      sliceRect = largeRect;
+    }
+    
+    UIView *v = [[UIView alloc] initWithFrame:sliceRect];
+    v.backgroundColor = color;
+    [self.view addSubview:v];
+    largeRect = leftoverRect;
+  }
   
 }
 
