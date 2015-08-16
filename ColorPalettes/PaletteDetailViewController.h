226c091e4e119946d76a0f11c6b28df6575ae7cd
Brian Broom <brian.broom@gmail.com>
2015-08-16 18:12:39 -0400

    Connect ViewController classes to storyboard View Controllers.

---
+++ ColorPalettes/PaletteDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  PaletteDetailViewController.h
+//  ColorPalettes
+//
+//  Created by Brian Broom on 8/16/15.
+//  Copyright (c) 2015 Brian Broom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaletteDetailViewController : UIViewController
+
+@end
---
+++ ColorPalettes/PaletteTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PaletteTableViewController.h
+//  ColorPalettes
+//
+//  Created by Brian Broom on 8/16/15.
+//  Copyright (c) 2015 Brian Broom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaletteTableViewController : UITableViewController
+
+@end
