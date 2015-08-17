c7164ef1e80d94bbc9d1dda01c8c08fb8f8199d2
Brian Broom <brian.broom@gmail.com>
2015-08-16 22:05:18 -0400

    Rework algorithm for displaying colors in palette to be simpler.

--- ColorPalettes/PaletteDetailViewController.m
+++ ColorPalettes/PaletteDetailViewController.m
@@ -28,22 +28,26 @@
     CGFloat height = largeRect.size.height;
     CGFloat width = largeRect.size.width;
     
-    if (color != self.selectedPalette.colors.lastObject) {
-      if (height > width) {
-        CGRectDivide(largeRect, &sliceRect, &leftoverRect, height / 2, CGRectMinYEdge);
-      } else {
-        CGRectDivide(largeRect, &sliceRect, &leftoverRect, width / 2, CGRectMinXEdge);
-      }
-    } else {
+    // sometimes start from top/left sometimes from bottom/right
+    CGRectEdge verticalEdge = (arc4random_uniform(2) == 0) ? CGRectMinYEdge : CGRectMaxYEdge;
+    CGRectEdge horizontalEdge = (arc4random_uniform(2) == 0) ? CGRectMinXEdge : CGRectMaxXEdge;
+    
+    CGRectEdge sliceFromEdge = (height > width) ? verticalEdge : horizontalEdge;
+    CGFloat sliceAmount = (height > width) ? height/2 : width/2;
+    
+    CGRectDivide(largeRect, &sliceRect, &leftoverRect, sliceAmount, sliceFromEdge);
+    
+    // escape hatch for last color in list
+    // can't divide here, or will have a white extra rect
+    if (color == self.selectedPalette.colors.lastObject) {
       sliceRect = largeRect;
     }
     
-    UIView *v = [[UIView alloc] initWithFrame:sliceRect];
-    v.backgroundColor = color;
-    [self.view addSubview:v];
+    UIView *colorVeiw = [[UIView alloc] initWithFrame:sliceRect];
+    colorVeiw.backgroundColor = color;
+    [self.view addSubview:colorVeiw];
     largeRect = leftoverRect;
   }
-  
 }
 
 @end
